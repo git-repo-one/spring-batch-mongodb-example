4ebd8414d40add65caaee082fbaa5730b22b5eea
user4 <user4@example.com>
2021-11-29 14:50:47 +0200

    add @Scheduled

--- src/main/java/com/example/launcher/MyJobLauncher.java
+++ src/main/java/com/example/launcher/MyJobLauncher.java
@@ -3,16 +3,25 @@ package com.example.launcher;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.example.domain.ReportConfig;
+import com.example.processor.ReportConfigItemProcessor;
 import com.example.repository.ReportConfigRepository;
 
+import lombok.extern.log4j.Log4j2;
+
 @Component
-public class MyJobLauncher implements CommandLineRunner {
+@Log4j2
+public class MyJobLauncher {
 
 	@Autowired
 	private JobLauncher jobLauncher;
@@ -22,18 +31,20 @@ public class MyJobLauncher implements CommandLineRunner {
 
 	@Autowired
 	private ReportConfigRepository reportConfigRepository;
-
-	@Override
-	public void run(String... args) throws Exception {
-
+	
+	@Scheduled(cron = "${jobA.cron}")
+	public void cronJob() {
+		
+		log.debug("inside cronJob()");
+		
 		// 1. clean collection
 		reportConfigRepository.deleteAll();
-		
+
 		//----------------------------------------
-		
+
 		// 2. insert data
 		for (int i = 1 ; i <= 50 ; i++) {
-		//for (int i = 17 ; i <= 18 ; i++) {
+			//for (int i = 17 ; i <= 18 ; i++) {
 
 			ReportConfig reportConfig = ReportConfig.builder()
 					.text("text_" + i)
@@ -43,16 +54,18 @@ public class MyJobLauncher implements CommandLineRunner {
 
 			reportConfigRepository.save(reportConfig);
 		}
-		
+
 		//----------------------------------------
-		
+
 		// 3. run job
 		JobParameters jobParameters = new JobParametersBuilder()
-										.addLong("time", System.currentTimeMillis())
-										.toJobParameters();
+				.addLong("time", System.currentTimeMillis())
+				.toJobParameters();
 
-		jobLauncher.run(job, jobParameters);
-		
-		System.exit(0);
+		try {
+			jobLauncher.run(job, jobParameters);
+		} catch (Exception e) {
+			log.error("fail to run job", e);
+		}
 	}
 }
